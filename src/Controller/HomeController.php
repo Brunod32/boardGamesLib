faf9b8ba08fe7264b07cd4a0af37feb1b6b4fab3
Bruno Dahlem <bruno.dahlem2@gmail.com>
2025-10-08 22:29:18 +0200

    Prepared for games search

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\CategoryRepository;
+use App\Repository\GameRepository;
+use App\Repository\NbPlayerRepository;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(
+        CategoryRepository $categoryRepository, 
+        NbPlayerRepository $nbPlayerRepository,
+        GameRepository $gameRepository
+    ): Response
     {
+        // 1. Récupérer toutes les catégories
+        $categories = $categoryRepository->findAll();
+        $nbPlayers = $nbPlayerRepository->findAll();
+        $games = $gameRepository->findAll();
+
+        // 2. Renvoyer la vue en passant les catégories
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'categories' => $categories,
+            'nbPlayers' => $nbPlayers,
+            'games' => $games,
         ]);
     }
 }
